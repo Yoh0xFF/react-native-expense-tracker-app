c33e54a2e211ecd80688515fd3d6659d65ee2e53
Ioram Gordadze <Yoh0xFF@gmail.com>
2023-01-21 15:33:33 +0000

    Add react navigation

---
+++ screens/ManageExpense.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+interface Props {}
+
+export default function ManageExpense({}: Props) {
+  return <View>Manage Expense Screen</View>;
+}
---
+++ screens/RecentExpenses.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+interface Props {}
+
+export default function AllExpenses({}: Props) {
+  return <View>Recent Expenses Screen</View>;
+}
